6b9a7edf02669c9d725afe09da598595116004de
Andrew Brooking <atbrooking.dev@gmail.com>
2019-08-05 13:47:32 -0500

    Added navbar with search features

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,24 @@
 // Start logic after page has loaded
 $(document).ready(function () {
-    // TODO
+
+    init();
 });
+
+function init() {
+
+    // Initialize input character counting
+    $('input#zip-input').characterCounter();
+
+    // Initialize date pickers
+    $('.datepicker').datepicker({
+        autoClose: true,
+        defaultDate: Date.now(),
+        setDefaultDate: true
+    });
+
+    // Initialize modals
+    $('.modal').modal();
+
+    // Intialize select fields
+    $('select').formSelect();
+}
