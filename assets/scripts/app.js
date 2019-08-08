67aff56f12cbf14e7ba3c14eae65614c9b19e8b6
Tierra <sr.undershaft@gmail.com>
2019-08-08 11:22:24 -0500

    logic for google places api added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,27 @@
+// ###################### Global Variables ##########################################
+// variable for holding user selected search radius
+var distanceInput;
+
+var searchArea = [29.7604,-95.3698]; //needs to be a latlng generated from user inputted zipcode
+
+// arrays of gplaces types organized by user search options
+const nightTypes = ["bar", "bakery", "casino", "night_club", "stadium", "bowling_alley", "art_gallery", "movie_theater"];
+const dayTypes = ["amusement_park", "aquarium", "art_gallery", "bowling_alley", "cafe", "campground", "park", "museum", "stadium"];
+const outdoorTypes = ["amusement_park", "campground", "park"];
+const allTypes = ["amusement_park", "aquarium", "art_gallery", "bar", "bowling_alley", "cafe", "campground", "casino", "movie_theater", "museum", "night_club", "park", "restaurant", "stadium"];
+
+//############################## App Logic #############################################
 // Start logic after page has loaded
 $(document).ready(function () {
 
     init();
+
+
+    // gets distance from user.  This should probably end up in an onclick function but is here because I needed it to happen after page load/init but before my function call
+    distanceInput = $("#range-filter").formSelect('getSelectedValues')[0];
+    //creates a radius in meters for the gPlacesSearch function call
+    var radiusInMeters = radiusConverter(distanceInput);
+    gPlacesSearch(searchArea[0], searchArea[1], ['restaurant'], radiusInMeters);
 });
 
 function init() {
@@ -24,70 +44,59 @@ function init() {
 }
 // ##################  API Info #############################
 //yelp
-const  yelpID = "8tbFFNcnX4YcPxbNA7DwBw"
+const yelpID = "8tbFFNcnX4YcPxbNA7DwBw"
 const yelpApiKey = "231r7Ia-ZXGh5J9wW4MA3DBGzycWROrufJz0I3wD_H1uCf16dba1IkRfPGyCzOSc9Cs8IbCyVMJcVT7oA0efxI756ydSvCXUA6pLTFyaRrjR3OgJzETvz68qRxdKXXYx"
 
-//google places 
-const gPlacesApiKey = "&key=AIzaSyCBjqQBqpsTRLKM_9Y0bYVCWNQVQwrve6o"
+// ######################  Google Places API Functions  ####################
 
-
-var gPlaceLoc = "location=-33.8670522,151.1957362"
-var gPlaceRadius = "&radius=50"
-var gPlaceType1 = "&types=restaurant"
-var name = "&name=harbour"
-
-var gPlaceURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + gPlaceLoc + gPlaceRadius + gPlaceType1 + gPlacesApiKey;
-
-var exampeURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise" + gPlacesApiKey;
-
-// $.ajax({
-//     url: exampeURL,
-//     method: "GET",
-//     datatype: "jsonp",
-// }).then(function (response) {
-//     console.log(response);
-
-// });
-//  logPlaceDetails()
-
-// function logPlaceDetails() {
-//     var service = new google.maps.places.PlacesService(document.getElementById('map'));
-//     service.getDetails({
-//       placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
-//     }, function (place, status) {
-//       console.log(place);
-//     });
-//   }
-fuckGoogle();
-
-var map;
-var service;
-var infowindow;
-
-function fuckGoogle() {
-  var pyrmont = new google.maps.LatLng(29.749907,-95.358421);
-
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
-    });
-
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    type: ['restaurant']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+// function to convert user input from miles to meters
+function radiusConverter(distanceMi) {
+    return distanceMi * 1609.34;
 }
 
-function callback(results, status) {
-  console.log(results);
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
+// function creats a request for API call 
+function gPlacesSearch(lat, lng, types, radius) {
+    var place = new google.maps.LatLng(lat, lng);
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: place,
+        zoom: 15
+    });
+
+    var request = {
+        location: place,
+        radius: radius,
+        type: types,
+    };
+//creates a google places service object to search
+    var service = new google.maps.places.PlacesService(map);
+//uses google places nearby search method to generate an API call and return a customized array of results
+    service.nearbySearch(request, function (results, status) {
+        console.log(results)
+        console.log(results[0].name, results[0].vicinity, results[0].opening_hours.isOpen())
+        var name;
+        var location;
+        var type;
+        var icon;
+        var gSearchResultsARR = [];
+        
+        for (var i = 0; i<results.length; i++) {
+            currentResult = results[i];
+
+            name = currentResult.name;
+            location = currentResult.vicinity;
+            type = currentResult.types;
+            icon = currentResult.icon;
+
+            var gSearchResultOBJ = {
+                name: name,
+                location: location,
+                type: type,
+                icon: icon,
+            }
+            gSearchResultsARR.push(gSearchResultOBJ);
+        }
+        console.log(gSearchResultsARR);
+        return gSearchResultsARR;
+    });
 }
